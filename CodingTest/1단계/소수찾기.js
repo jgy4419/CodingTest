f7d5c3c4b070602e5758b81e84fd3a107e8a5ae1
jgy4419 <jgy_98@naver.com>
2022-09-20 14:36:13 +0900

    .

--- CodingTest/1단계/소수찾기.js
+++ CodingTest/1단계/소수찾기.js
@@ -1,4 +1,14 @@
 function solution(n) {
     var answer = 0;
-    return answer;
+    let count = 0;
+    // console.log('numArr', numArr);
+    for (let i = 2; i <= n; i++){
+        if (i % 2 === 0 || i % 3 !== 0) {
+            console.log('?', i);
+            count++;
+        }
+    }
+    return count;
 }
+
+console.log(solution(10));
