36f4f65a1e20dc4ead04a07edb5c98e512235556
jgy4419 <jgy_98@naver.com>
2022-10-28 23:40:55 +0900

    .

--- abc.js
+++ abc.js
@@ -1,21 +1,28 @@
-// 나머지로 나온 값 변수에 저장
-// 나눠서 나온 값 변수에 따로 저장
-// 반복해서 나온 값 변수에 저장
-
-// a는 나눌 기준, b는 마트에서 주는 콜라 병 수, n은 가지고 있는 빈 병 총 개수
-function solution(a, b, n) {
-    var answer = 0;
-    let remainder = 0;
-    let divided = 0;
-    while (true) {
-        divided = parseInt((n % a)) 
-        answer += (parseInt(n / a) * b);
-        n = parseInt(n / a) + divided;
-        if (n < a) {
-            break;
-        } 
+class Rectangle{
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
     }
-    return answer;
+    getArea() {
+        return this.width * this.height;
+    }
+    toString() {
+        return `width - ${this.width}. height = ${this.height}`;
+    }
 }
 
-console.log(solution(3, 1, 20));
+class colorRectangle extends Retangle{
+    constructor(width, height, color) {
+        super(width, height);
+        this.color = color;
+    }
+    toString() {
+        return super.toString() + `, color = ${this.color}`;
+    }
+}
+
+const colorRectangle = new ColorRectangle(2, 4, 'red');
+console.log(colorRectangle);
+
+console.log(colorRectangle.getArea());
+console.log(colorRectangle.toString());
--- test.ts
+++ test.ts
@@ -1,7 +1,13 @@
-class PersonExtends<T extends string | number>{
-    private _name: T;
-
-    constructor(name: T) {
-        this._name = name;
+class Person {
+    name: string;
+    inputName: string;
+    constructor(a: string) {
+        this.name = 'kim';
+        this.inputName = a;
     }
 }
+let person1 = new Person('kim');
+let person2 = new Person('park');
+
+console.log(person1);
+console.log(person2);
