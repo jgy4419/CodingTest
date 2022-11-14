7e64fabbc95674ae618cd72829764b4760dcdcd8
jgy4419 <jgy_98@naver.com>
2022-11-14 19:24:30 +0900

    .

---
+++ CodingTest/1단계/(바로 풀기..)성격유형검사하기.js
@@ -0,0 +1,7 @@
+function solution(survey, choices) {
+    var answer = '';
+    return answer;
+}
+
+console.log(solution(["AN", "CF", "MJ", "RT", "NA"], [5, 3, 2, 7, 5]));
+// result "TCMA"
---
+++ CodingTest/2단계/행렬의 곱셈.js
@@ -0,0 +1,20 @@
+function solution(arr1, arr2) {
+    const [row, col] = [arr1.length, arr2[0].length];
+    console.log(`row ${row} col ${col}`);
+    var answer = [[]];
+    var count = 0;
+    console.log(arr1.map((value, i_index) => {
+        count = 0;
+        value.map((val, j_index) => { 
+            // console.log(val)
+            console.log('val', val);
+            console.log('val2', arr2[i_index][col - 1]);
+            console.log('res', val * arr2[i_index][j_index]);
+            count++;
+            // console.log(val * arr2[j_index]);
+        })
+    }))
+    return answer;
+}
+
+console.log(solution([[2, 3, 2], [4, 2, 4], [3, 1, 4]], [[5, 4, 3], [2, 4, 1], [3, 1, 1]]));
