4d7471e01d5f79e667d0fbc8c15d483c17deef53
jgy4419 <jgy_98@naver.com>
2022-11-24 10:02:05 +0900

    221124

--- CodingTest/1단계/콜라문제.js
+++ CodingTest/1단계/콜라문제.js
@@ -1,21 +1,10 @@
-// 나머지로 나온 값 변수에 저장
-// 나눠서 나온 값 변수에 따로 저장
-// 반복해서 나온 값 변수에 저장
-
-// a는 나눌 기준, b는 마트에서 주는 콜라 병 수, n은 가지고 있는 빈 병 총 개수
 function solution(a, b, n) {
-    var answer = 0;
-    let remainder = 0;
-    let divided = 0;
-    while (true) {
-        divided = parseInt((n % a)) 
-        answer += (parseInt(n / a) * b);
-        n = parseInt(n / a) + divided;
-        if (n < a) {
-            break;
-        } 
+    let acc = 0; // 콜라 누적
+    while (n >= a) {
+        acc += Math.floor(n / a) * b; // 소숫점 제거//
+        n = Math.floor(n / a) * b + n % a;
     }
-    return answer;
+    return acc;
 }
 
 console.log(solution(3, 1, 20));
