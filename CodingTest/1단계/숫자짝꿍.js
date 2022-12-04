9688bae8a068e7f7b6c5a3d3b202e618bd00f569
jgy4419 <jgy_98@naver.com>
2022-12-04 12:23:41 +0900

    221204

--- CodingTest/1단계/숫자짝꿍.js
+++ CodingTest/1단계/숫자짝꿍.js
@@ -1,19 +1,23 @@
+// 다른사람 풀이
 // function solution(X, Y) {
-//     let resArr = [];
-//     for (let i = 0; i < X.length; i++){
-//         resArr.push(Y.split('').filter((num, index) => {
-//             return num === X[i];
-//         }));
+//     let answer = '';
+//     X = X.split('');
+//     Y = Y.split('');
+
+//     for (let i = 0; i < 10; i++){
+//         const curX = X.filter(a => Number(a) === i).length;
+//         const curY = Y.filter(a => Number(a) === i).length;
+
+//         console.log(curX, curY);
+//         answer += String(i).repeat(Math.min(curX, curY));
 //     }
-//     var answer = new Set(resArr.join('').replace(/,/gi, ''));
-//     console.log(answer);
-//     return answer === {} ? "-1" : [...answer].sort((a, b) => b - a).join('');
+//     if (answer === '') return "-1";
+//     if (Number(answer) === 0) return '0';
+
+//     return answer.split('').sort((a, b) => Number(b) - Number(a)).join("");
 // }
 
-// console.log(solution("100", "2345"));
-
-
-// 시간 초과
+// 나의 풀이 (시간초과)
 function solution(X, Y) {
     var answer = [];
     let arr = Y.split('');
@@ -28,11 +32,5 @@ function solution(X, Y) {
     if (parseInt(answer.join('')) === 0) return '0';
     return answer.length === 0 ? '-1' : answer.sort((a, b) => b - a).join('');
 }
-
-// function solution(X, Y) {
-//     var answer = [];
-//     // for()
-//     return anwer;
-// }
 
 console.log(solution("100", "203045"));
