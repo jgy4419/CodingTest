bda8a5993615e2367112fdd9419a48a2049f8a63
jgy4419 <jgy_98@naver.com>
2023-01-08 16:25:09 +0900

    230108 - 합병정렬 구현

---
+++ udemy/합병 정렬/합병 정렬 구현하기.js
@@ -0,0 +1,33 @@
+function mergeSort(arr) {
+    if (arr.length <= 1) return arr;
+    let mid = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0, mid));
+    let right = mergeSort(arr.slice(mid));
+    return merge(left, right);
+}
+
+function merge(arr1, arr2) {
+    console.log(arr1, arr2);
+    let results = [];
+    let [i, j] = [0, 0];
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            results.push(arr1[i]);
+            i++;
+        } else {
+            results.push(arr2[j]);
+            j++;
+        }
+    }
+    while (i < arr1.length) {
+        results.push(arr1[i]);
+        i++;
+    }
+    while (j < arr2.length) {
+        results.push(arr2[j]);
+        j++;
+    }
+    return results;
+}
+
+console.log(mergeSort([5, 30, 12, 4, 25, 34, 1, 43, 100, 20, 10]));
