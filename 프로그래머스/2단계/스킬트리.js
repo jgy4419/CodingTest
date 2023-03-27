bb5203b314a4bf00ebc3841d85d5bdf1f96d609f
정규영 <jgy_98@naver.com>
2023-03-27 15:02:28 +0900

    230327 - 스킬트리

--- 프로그래머스/2단계/스킬트리.js
+++ 프로그래머스/2단계/스킬트리.js
@@ -1,42 +1,40 @@
+// 내 풀이
 function solution(skill, skill_trees) {
+    let copySkill = skill.split('');
     var answer = 0;
-    skill_trees.forEach(item => {
-        console.log(item);
-        let copyItem = [...item]
-        let copySkill = [...skill];
-        let i = 0;
-        while(i !== copyItem.length){ 
-            console.log(copySkill, copyItem[0]);
-            if(copySkill.includes(copyItem[0])) {
-                console.log(copySkill, copyItem[0], copySkill[0]);
-                if(copyItem[0] === copySkill[0]){
-                    copySkill.splice(0, 1);
-                    copyItem.splice(0, 1);
-                    if(copySkill.length === 0 || copyItem.length === 0) {
+    
+    for(let i = 0; i < skill_trees.length; i++) {
+        for(let j = 0; j < skill_trees[i].length; j++) {
+            if(copySkill.includes(skill_trees[i][j])){
+                if(skill_trees[i][j] === copySkill[0]){
+                    copySkill.shift();
+                    if(copySkill.length === 0 || j === skill_trees[i].length - 1) {
                         answer++;
-                        console.log(answer);
                         break;
                     }
-                }else {
-                    break;
-                }
-            }else copyItem.splice(0, 1);
-            i++;
+                }else break;
+            }else if(j === skill_trees[i].length - 1) answer++;
         }
-    })
+        copySkill = skill.split('');
+    }
     return answer;
 }
 
-console.log(solution("CBD", ["BACDE", "CBADF", "AECB", "BDA"])); // 2
+
+console.log(solution("CBD",  ["AEF", "ZJW"])); // 2
+console.log(solution("CBD", ["BACDE", "CBADF", "AECB", "BDA"]))
+
+
+
+
 
 //다른 사람 풀이
-// function solution(skill, skill_trees) {
-//     var regex = new RegExp(`[^${skill}]`, 'g');
-
-//     return skill_trees
-//         .map((x) => x.replace(regex, ''))
-//         .filter((x) => {
-//             return skill.indexOf(x) === 0 || x === "";
-//         })
-//         .length
-// }
+function solution(skill, skill_trees){
+    const restRemoveRegex = new RegExp('[^' + skill + ']', 'gi');
+    return skill_trees.filter(v => {
+        const tempSkill = v.replace(restRemoveRegex, '');
+        // startWith() : 어떤 문자열이 특정 문자로 시작하는지 확인하여 결과 true 혹은 false로 반환한다.
+        // tempSkill이 skill 기준 시작하는 문자라면? true. (length 추가)
+        return skill.startsWith(tempSkill);
+    }).length;
+}
