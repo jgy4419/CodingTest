32a4e5b9179d40ecb3519a8e6e6543fdf17550c1
정규영 <jgy_98@naver.com>
2023-05-09 20:48:37 +0900

    230509 - 정렬 문제 풀이 3

--- 패스트캠퍼스 코딩테스트/정렬 문제 풀이.js
+++ 패스트캠퍼스 코딩테스트/정렬 문제 풀이.js
@@ -39,4 +39,75 @@ function solution(arr) {
     return resArr;
 }
 
-console.log(solution(['but', 'i', 'i', 'wont', 'hesitate', 'no', 'more']));   
+console.log(solution(['but', 'i', 'i', 'wont', 'hesitate', 'no', 'more']));
+
+
+// 정렬 문제 풀이
+function solution(leng, arr) {
+    let answer = [];
+    let newArr = [];
+    for(let i = 0; i < leng; i++) {
+        for(let j = 0; j < leng; j++) {
+            if(arr[i] > arr[j] && !newArr.includes(arr[j])){
+                newArr.push(arr[j]);
+            }
+        }
+        answer.push(newArr.length);
+        newArr = [];
+    }
+    return answer;
+}
+
+// 강사님 풀이
+function solution(arr) {
+    let answer = "";
+    let uniqueArray = [...new Set(arr)];
+    uniqueArray.sort((a, b) => a - b);
+
+    let myMap = new Map();
+    for(let i = 0; i < uniqueArray.length; i++) {
+        myMap.set(uniqueArray[i], i);
+    }
+    for(let x of arr) answer += myMap.get(x);
+
+    return answer;
+}
+
+console.log(solution([2, 4, -10, 4, -9]));
+console.log(solution([1000, 999, 1000, 999, 1000, 999]));
+
+// 나이순 정렬
+function solution(arr){
+    arr.sort((a, b) => a[0] - b[0])
+    return arr;
+}
+
+console.log(solution([[21, "Junkyu"], [21, "Dohyun"], [20, "Sunyoung"]]));
+
+// 강사님 풀이
+function solution() {
+    let arr = [];
+
+    for(let i = 0; i <= n; i++) {
+        let age = Number(input[i].split(' ')[0]);
+        let name = input[i].split(' ')[1];
+        arr.push([age, name]);
+    }
+
+    arr.sort((a, b) => a[0] - b[0]);
+
+    let answer = '';
+    for(let x of arr) answer += x[0] + " " + x[1] + "\n";
+    console.log(answer);
+}
+
+// 소트인사이드
+function solution(num){
+    let numChangedStr = num.toString().split('');
+    numChangedStr.sort((a, b) => Number(b) - Number(a));
+    return numChangedStr.join('');
+}
+
+console.log(solution(2143));
+console.log(solution(999998999));
+console.log(solution(9792382));
